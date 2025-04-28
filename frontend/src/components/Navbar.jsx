595c3240364817f8be350b6c32c635c6e1319800
Swagzilla007 <158685149+Swagzilla007@users.noreply.github.com>
2025-04-28 23:41:05 +0530

    restructure App component and add Layout and Navbar components for improved routing and UI

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Cleaning Service
+        </Typography>
+        <Box>
+          <Button color="inherit" component={RouterLink} to="/">
+            Bookings
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/services">
+            Services
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/register">
+            Register
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
